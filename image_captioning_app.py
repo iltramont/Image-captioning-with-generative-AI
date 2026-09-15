81c67c92f5f809eafa3afd327fd40d5b47775de8
Luca Tramonti <124902413+iltramont@users.noreply.github.com>
2026-09-15 16:08:44 +0200

    Fix formatting and clean up comments in image_captioning_app

--- image_captioning_app.py
+++ image_captioning_app.py
@@ -12,23 +12,21 @@ def caption_image(input_image: np.ndarray):
     raw_image = Image.fromarray(input_image).convert('RGB')
 
     # Process the image
-    # text = "the image of"
     inputs = processor(raw_image, return_tensors="pt")
 
     # Generate a caption for the image
-    out = model.generate(**inputs, max_length=50)
+    out = model.generate(**inputs,max_length=50)
 
-    # Decode the generated tokens to text and store it into `caption`
+    # Decode the generated tokens to text
     caption = processor.decode(out[0], skip_special_tokens=True)
 
     return caption
-
 
 iface = gr.Interface(
     fn=caption_image,
     inputs=gr.Image(),
     outputs="text",
     title="Image Captioning",
-    description="This is a simple web app for generating captions for images using a trained model."
-)
+    description="This is a simple web app for generating captions for images using a trained model." )
 
+iface.launch()
